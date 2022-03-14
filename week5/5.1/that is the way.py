2778096d8f32ea59c486a2d72acbba59a41184fb
Eliad Karni <EliadK995@Gmail.com>
2022-03-14 14:30:17 +0200

    5.1 syntax fix. 5.2 beginning.

--- week5/5.1/that is the way.py
+++ week5/5.1/that is the way.py
@@ -2,18 +2,19 @@ import os
 """
 That's the way.
 By: Eliad Karni.
-ID: 206329104.
 
 The project receive a file path from the user and prints a list of the file in the received directory,
 which their names starts with PREFIX value.
 
 used websites help:
-os usage: Stuck Overfolw -> https://stackoverflow.com/questions/2759323/how-can-i-list-the-contents-of-a-directory-in-python.
+os usage: Stuck Overflow -> https://stackoverflow.com/questions/2759323/how-can-i-list-the-contents-of-a-directory-in-python.
 """
-#------------------------------- consts section -------------------------------
+# ------------------------------ consts section -------------------------------
 PREFIX = "deep"
 
-#------------------------------ function section ------------------------------
+# ----------------------------- function section ------------------------------
+
+
 def get_files_list(path:str):
     """
     The function receives a directory path, and returns a list of
@@ -23,5 +24,7 @@ def get_files_list(path:str):
     """
     return [file for file in os.listdir(path) if file.startswith(PREFIX)]
 
-#-------------------------------- main section --------------------------------
+# ------------------------------- main section --------------------------------
+
+
 print(get_files_list(input("please enter dir path: ")))
