a962fc59fd585d5e4292036bdd1eb0edfd92a4b2
Eliad Karni <EliadK995@Gmail.com>
2022-03-15 13:49:27 +0200

    Documentation fix

--- week5/5.3/whisper.py
+++ week5/5.3/whisper.py
@@ -1,16 +1,21 @@
 """
-whisper!.
+whisper.
 By: Eliad Karni.
 
 The feature decode messages which contained in the file "logo.jpg".
 
-used the help of the websites:
-binary read -> https://pythonguides.com/python-read-a-binary-file/
-ignore decode errors -> https://docs.python.org/3/library/codecs.html
+Used the help of the websites:
+Binary read -> https://pythonguides.com/python-read-a-binary-file/
+Ignore decode errors -> https://docs.python.org/3/library/codecs.html
 """
 
 
-def get_a_whisper():
+def get_a_whisper() -> str:
+    """
+    The function is a iterate generator which decode a whisper from the file "logo.jpg".
+    1 message each time.
+    :return: a decoded word from the file "logo.jpg".
+    """
     with open('logo.jpg', 'br') as file_reader:
         decoded_word = ''
         char = file_reader.read(1)
