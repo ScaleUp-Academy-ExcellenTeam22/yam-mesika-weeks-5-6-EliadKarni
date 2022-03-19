afc2fbd9e947ace8de0bf79d8296d988255c7876
Eliad Karni <EliadK995@Gmail.com>
2022-03-19 12:24:58 +0200

    Whisper feacher added

---
+++ week5/5.3/whisper.py
@@ -0,0 +1,35 @@
+"""
+whisper.
+By: Eliad Karni.
+The feature decode messages which contained in the file "logo.jpg".
+Used the help of the websites:
+Binary read -> https://pythonguides.com/python-read-a-binary-file/
+Ignore decode errors -> https://docs.python.org/3/library/codecs.html
+"""
+
+
+def get_a_whisper() -> str:
+    """
+    The function is a iterate generator which decode a whisper from the file "logo.jpg".
+    1 message each time.
+    :return: a decoded word from the file "logo.jpg".
+    """
+    with open('logo.jpg', 'br') as file_reader:
+        decoded_word = ''
+        char = file_reader.read(1)
+        while char:
+            char = char.decode(errors='ignore')
+            if char is '!':
+                if len(decoded_word) > 4:
+                    yield decoded_word
+                decoded_word = ''
+            elif char.islower():
+                decoded_word += char
+            else:
+                decoded_word = ''
+            char = file_reader.read(1)
+
+
+if __name__ == '__main__':
+    for word in get_a_whisper():
+        print(word)
