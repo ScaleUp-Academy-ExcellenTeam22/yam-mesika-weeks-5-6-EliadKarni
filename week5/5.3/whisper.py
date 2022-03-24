9e8eb669eaac72f9fdd1e2381a614f93bca4ab34
Eliad Karni <EliadK995@Gmail.com>
2022-03-24 14:51:27 +0200

    Review fixes

--- week5/5.3/whisper.py
+++ week5/5.3/whisper.py
@@ -1,7 +1,7 @@
 """
 whisper.
 By: Eliad Karni.
-The feature decode messages which contained in the file "logo.jpg".
+The code decode messages which contained in the file "logo.jpg".
 Used the help of the websites:
 Binary read -> https://pythonguides.com/python-read-a-binary-file/
 Ignore decode errors -> https://docs.python.org/3/library/codecs.html
@@ -10,26 +10,24 @@ Ignore decode errors -> https://docs.python.org/3/library/codecs.html
 
 def get_a_whisper() -> str:
     """
-    The function is a iterate generator which decode a whisper from the file "logo.jpg".
-    1 message each time.
+    The function is a generator which decode a whisper from the file "logo.jpg".
+    One message each call.
     :return: a decoded word from the file "logo.jpg".
     """
-    with open('logo.jpg', 'br') as file_reader:
-        decoded_word = ''
-        char = file_reader.read(1)
-        while char:
-            char = char.decode(errors='ignore')
-            if char is '!':
-                if len(decoded_word) > 4:
-                    yield decoded_word
-                decoded_word = ''
-            elif char.islower():
-                decoded_word += char
-            else:
-                decoded_word = ''
-            char = file_reader.read(1)
+    with open("logo.jpg", "br") as file_reader:
+        decoded_word = ""
+        for line in file_reader:
+            for char in line.decode(errors="ignore"):
+                if char == "!":
+                    if len(decoded_word) > 4:
+                        yield decoded_word
+                    decoded_word = ""
+                elif char.islower():
+                    decoded_word += char
+                else:
+                    decoded_word = ""
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     for word in get_a_whisper():
         print(word)
